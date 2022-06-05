88e72b50d388f170133323375ed861cca13f9282
Mathieu Acthernoene <zoontek@gmail.com>
2022-06-05 23:28:39 +0200

    Add match / tap / toUndefined / toNull

--- src/Option2.ts
+++ src/Option2.ts
@@ -4,14 +4,14 @@ interface OptionProto<A> {
    *
    * (Option\<A>, A => B) => Option\<B>
    */
-  map<B>(this: Option<A>, f: (value: A) => B): Option<B>;
+  map<B>(this: Option<A>, func: (value: A) => B): Option<B>;
 
   /**
    * Returns the Option containing the value from the callback
    *
    * (Option\<A>, A => Option\<B>) => Option\<B>
    */
-  flatMap<B>(this: Option<A>, f: (value: A) => Option<B>): Option<B>;
+  flatMap<B>(this: Option<A>, func: (value: A) => Option<B>): Option<B>;
 
   /**
    * Return the value if present, and the fallback otherwise
@@ -19,6 +19,31 @@ interface OptionProto<A> {
    * (Option\<A>, A) => A
    */
   getWithDefault(this: Option<A>, defaultValue: A): A;
+
+  /**
+   * Explodes the Option given its case
+   */
+  match<B>(
+    this: Option<A>,
+    config: { Some: (value: A) => B; None: () => B },
+  ): B;
+
+  /**
+   * Runs the callback and returns `this`
+   */
+  tap(this: Option<A>, func: (option: Option<A>) => unknown): Option<A>;
+
+  /**
+   * Converts the Option\<A> to a `A | undefined`
+   */
+  toUndefined(this: Option<A>): A | undefined;
+
+  /**
+   * Converts the Option\<A> to a `A | null`
+   */
+  toNull(this: Option<A>): A | null;
+
+  // TODO: Add toResult
 
   /**
    * Typeguard
@@ -52,18 +77,37 @@ interface None<A> extends OptionProto<A> {
 export type Option<A> = Some<A> | None<A>;
 
 const optionProto: OptionProto<unknown> = {
-  map(f) {
+  map(func) {
     return this.tag === "Some"
-      ? Some(f(this.value))
-      : (this as Option<ReturnType<typeof f>>);
+      ? Some(func(this.value))
+      : (this as Option<ReturnType<typeof func>>);
   },
 
-  flatMap(f) {
-    return this.tag === "Some" ? f(this.value) : (this as ReturnType<typeof f>);
+  flatMap(func) {
+    return this.tag === "Some"
+      ? func(this.value)
+      : (this as ReturnType<typeof func>);
   },
 
   getWithDefault(defaultValue) {
     return this.tag === "Some" ? this.value : defaultValue;
+  },
+
+  match(config) {
+    return this.tag === "Some" ? config.Some(this.value) : config.None();
+  },
+
+  tap(func) {
+    func(this);
+    return this;
+  },
+
+  toUndefined() {
+    return this.tag === "Some" ? this.value : undefined;
+  },
+
+  toNull() {
+    return this.tag === "Some" ? this.value : null;
   },
 
   isSome() {
@@ -83,7 +127,9 @@ const someProto: SomeProto<unknown> = {
   },
 };
 
-const noneProto: NoneProto<unknown> = optionProto;
+const noneProto: NoneProto<unknown> = {
+  ...optionProto,
+};
 
 const Some = <A>(value: A): Option<A> => {
   const some = Object.create(someProto) as Some<A>;
