8c77282d15c2cff047ef978b58ba000a08f7de65
Mathieu Acthernoene <zoontek@gmail.com>
2022-06-05 17:56:30 +0200

    WIP

---
+++ src/Option2.ts
@@ -0,0 +1,99 @@
+interface OptionCommon<A> {
+  /**
+   * Returns the Option containing the value from the callback
+   *
+   * (Option\<A>, A => B) => Option\<B>
+   */
+  map<B>(this: Option<A>, f: (value: A) => B): Option<B>;
+
+  /**
+   * Typeguard
+   */
+  isSome(this: Option<A>): this is Some<A>;
+
+  /**
+   * Typeguard
+   */
+  isNone(this: Option<A>): this is None<A>;
+}
+
+const getOptionCommon = <A>(): OptionCommon<A> => ({
+  map<B>(this: Option<A>, f: (value: A) => B): Option<B> {
+    return this.tag === "Some"
+      ? Some(f(this.value))
+      : (this as unknown as Option<B>);
+  },
+
+  isSome(this: Option<A>) {
+    return this.tag === "Some";
+  },
+
+  isNone(this: Option<A>) {
+    return this.tag === "None";
+  },
+});
+
+type Some<A> = OptionCommon<A> & {
+  tag: "Some";
+  value: A;
+
+  /**
+   * Returns the value. Use within `if (option.isSome()) { ... }`
+   */
+  get(this: Some<A>): A;
+};
+
+type None<A> = OptionCommon<A> & {
+  tag: "None";
+};
+
+export type Option<A> = Some<A> | None<A>;
+
+const Some = <A>(value: A): Some<A> => ({
+  ...getOptionCommon<A>(),
+
+  tag: "Some",
+  value,
+
+  get(this: Some<A>): A {
+    return this.value;
+  },
+});
+
+const None = <A>(): None<A> => ({
+  ...getOptionCommon<A>(),
+
+  tag: "None",
+});
+
+export const Option = {
+  /**
+   * Create an Option.Some value
+   */
+  Some: <A>(value: A): Option<A> => Some(value),
+
+  /**
+   * Create an Option.None value
+   */
+  None: <A>(): Option<A> => None(),
+
+  /**
+   * Create an Option from a nullable value
+   */
+  fromNullable: <A>(nullable: A | null | undefined): Option<A> =>
+    nullable == null ? None<A>() : Some<A>(nullable),
+
+  /**
+   * Create an Option from a null | value
+   */
+  fromNull: <A>(nullable: A | null): Option<A> =>
+    nullable === null ? None<A>() : Some<A>(nullable),
+
+  /**
+   * Create an Option from a undefined | value
+   */
+  fromUndefined: <A>(nullable: A | undefined): Option<A> =>
+    nullable === undefined ? None<A>() : Some<A>(nullable),
+
+  // TODO: Implements the rest
+};
---
+++ src/Option2Example.ts
@@ -0,0 +1,3 @@
+import { Option } from "../dist/Boxed";
+
+const option: Option<string> = Option.fromNull("foo");
